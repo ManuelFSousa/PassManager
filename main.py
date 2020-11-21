efc4c601062d0b57b831769f6e9bc65537ab7a8b
Manuel Sousa <manuelsousa@Air-de-Manuel.Home>
2020-11-21 13:08:41 +0000

    Início Contrução UI

---
+++ main.py
@@ -0,0 +1,51 @@
+#Comment
+
+def login():
+    print(""" 
+               .--.
+              /.-. '----------.
+              \'-' .--"--""-"-'
+               '--' 
+    """)
+    print("Gestor de Palavras Passes V1")
+
+    user = input("Utilizador: ")
+    password = input("Password: ")
+
+    FILE
+
+
+def imprimirMenu():
+    print("\nEscolha o que deseja fazer:")
+    print("1 - Guardar Nova Palavra Passe")
+    print("2 - Pedir Palavra Passe")
+    print("3 - Mudar utilizador")
+    print("4 - Fechar Programa")
+
+def savePass():
+    print("algo")
+
+
+def main():
+    if login() == 1:
+        print("Login bem sucedido!")
+    else:
+        print("Login inexistente!")
+        return
+
+    escolha = 1
+    while escolha > 0 or escolha < 6:
+        imprimirMenu()
+        escolha = int(input("\nA sua opção: "))
+        if escolha == 1:
+            savePass()
+        elif escolha == 5:
+            break
+
+
+
+
+if __name__ == "__main__":  #Quando abro diretamente este ficheiro, a variável especial "__name" tem a string "__main__"
+    main()                  #atribuída. Isto acontece por definição. Caso este módulo seja chamado por outro programa,
+                            #a variável "__name__" fica com a string igual ao nome (neste caso seria só "main").
+
